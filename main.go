64e3a52b09bb3fff55d93b73dc419435e43f772d
konjoot <konjoot@gmail.com>
2015-05-22 07:27:04 +0300

    working on POST method for book_usage_statistic_items

--- main.go
+++ main.go
@@ -1,18 +1,38 @@
 package main
 
 import (
-	"net/http"
-	"github.com/gin-gonic/gin"
+  // "net/http"
+  "github.com/gin-gonic/gin"
+  "github.com/gin-gonic/gin/binding"
 )
 
+type BookUsageStatisticItemForm struct {
+  ProfileId int `form:"profile_id" json:"ProfileId" binding required`
+  BookItemId int `form:"book_item_id" json:"BookItemId" binding required`
+}
+
+type BookUsageStatisticItem struct {
+  Id int `db:"id"`
+  ProfileId int `db:"profile_id"`
+  BookItemId int `db:"book_item_id"`
+}
+
 func main() {
-	r := gin.Default()
+  r := gin.Default()
 
-	r.POST("/book_usage_statistic_items", func(c *gin.Context){
-		c.String(http.StatusOK, "pong")
-	})
+  r.POST("/book_usage_statistic_items", func(c *gin.Context){
 
-	r.Run(":8080")
+    var form BookUsageStatisticItemForm
+
+    c.BindWith(&form, binding.MultipartForm)
+
+    c.JSON(200, &form)
+
+    // var Busi BookUsageStatisticItem
+    // curl -v --form profile_id=1 --form book_item_id=2 http://localhost:8080/book_usage_statistic_items
+  })
+
+  r.Run(":8080")
 }
 
 
