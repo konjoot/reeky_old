21f30613834750cbf8fbaf47830effca1312ffca
konjoot <konjoot@gmail.com>
2015-05-23 19:01:39 +0300

    simple form validation

--- main.go
+++ main.go
@@ -1,17 +1,16 @@
 package main
 
 import (
-	// "net/http"
-	"github.com/gin-gonic/gin"
-	"github.com/gin-gonic/gin/binding"
+	"github.com/konjoot/gin"
+	"net/http"
 )
 
-type BookUsageStatisticItemForm struct {
-	ProfileId  int `form:"profile_id" json:"ProfileId" binding required`
-	BookItemId int `form:"book_item_id" json:"BookItemId" binding required`
+type BookUsageStatisticItem struct {
+	ProfileId  int `form:"profile_id" binding:"required"`
+	BookItemId int `form:"book_item_id" binding:"required"`
 }
 
-type BookUsageStatisticItem struct {
+type BookUsageStatisticItemDB struct {
 	Id         int `db:"id"`
 	ProfileId  int `db:"profile_id"`
 	BookItemId int `db:"book_item_id"`
@@ -22,13 +21,14 @@ func main() {
 
 	r.POST("/book_usage_statistic_items", func(c *gin.Context) {
 
-		var form BookUsageStatisticItemForm
+		var form BookUsageStatisticItem
 
-		c.BindWith(&form, binding.MultipartForm)
+		if errs := c.Bind(&form); errs != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"errors": errs})
+			return
+		}
 
 		c.JSON(200, &form)
-
-		// var Busi BookUsageStatisticItem
 		// curl -v --form profile_id=1 --form book_item_id=2 http://localhost:8080/book_usage_statistic_items
 	})
 
